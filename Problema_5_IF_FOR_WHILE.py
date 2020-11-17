c4bcbdc587ffe287df00bb324440b8ed2b9eab0e
CodreanuAlexandru <70651712+CodreanuAlexandru@users.noreply.github.com>
2020-11-17 09:06:14 +0200

    Add files via upload

---
+++ Problema_5_IF_FOR_WHILE.py
@@ -0,0 +1,25 @@
+n=eval(input('Introduceti anul:'))
+if (n-1)%12==0:
+    print(n,'este anul cucosului')
+elif n%12==0:
+    print(n,'este anul maimutei')
+elif n%11==0:
+    print(n,'este anul oii')
+elif n%10==0:
+    print(n,'este anul calului')
+elif n%9==0:
+    print(n,'este anul sarpelui')
+elif n%8==0:
+    print(n,'este anul dragonului')
+elif n%7==0:
+    print(n,'este anul iepurilui')
+elif n%6==0:
+    print(n,'este anul tigrului')
+elif n%5==0:
+    print(n,'este anul boului')
+elif n%4==0:
+    print(n,'este anul sobolanului')
+elif n%3==0:
+    print(n,'este anul porcului')
+elif n%2==0:
+    print(n,'este anul cainelui')
