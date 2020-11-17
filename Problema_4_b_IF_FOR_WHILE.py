d1cf49638893bc2ee7e6e94487a17558aa2d20f9
CodreanuAlexandru <70651712+CodreanuAlexandru@users.noreply.github.com>
2020-11-17 08:43:22 +0200

    Add files via upload

---
+++ Problema_4_b_IF_FOR_WHILE.py
@@ -0,0 +1,6 @@
+from fractions import Fraction
+a=eval(input('Introduceti primul numitor:'))
+b=eval(input('Introduceti primul numarator:'))
+c=eval(input('Introduceti al doilea numitor:'))
+d=eval(input('Introduceti al doilea numarator:'))
+print(Fraction(a,b)*Fraction(c,d))
